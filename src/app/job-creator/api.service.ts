8570b49a45f05f9a6355bc347677451486db4126
DasRohr <toni.warnecke@mailbox.org>
2018-05-28 23:32:08 +1200

    adding api service for omniAPI

---
+++ src/app/job-creator/api.service.ts
@@ -0,0 +1,26 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { environment } from "../../environments/environment";
+
+@Injectable()
+export class OmniApiService {
+  private apiUrl = 'http://' + environment.api.host + ':' + environment.api.port;
+  albartDataReceived = new EventEmitter<{}>();
+  albDataReceived = new EventEmitter<{}>();
+
+  constructor(private http :HttpClient) {}
+
+  getExistingAlbart() {
+    this.http.get( this.apiUrl + '/fs/alb' ).subscribe( ( response :any ) => {
+      this.albartDataReceived.emit(response.data);
+    });
+  }
+
+  getExistingAlb( albart :string ) {
+    console.log('triggered get Album with ', albart)
+    this.http.get( this.apiUrl + '/fs/albart' ).subscribe ( ( response :any ) => {
+      this.albDataReceived.emit( response.data );
+    })
+  }
+}
