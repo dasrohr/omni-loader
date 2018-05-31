6e0785828488fddea0c37c9e623d6bb1bf5b815e
DasRohr <toni.warnecke@mailbox.org>
2018-06-01 00:27:57 +1200

    make the selection work with video servide further change template to mat-design

--- src/app/job-creator/job-selector/job-selector.component.ts
+++ src/app/job-creator/job-selector/job-selector.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Video } from '../video.model';
 import { VideoService } from '../video.service';
+import { OmniApiService } from '../api.service';
 
 @Component({
   selector: 'app-job-selector',
@@ -15,13 +16,18 @@ export class JobSelectorComponent implements OnInit {
 
   loading :string;
 
-  constructor(private videoService :VideoService) { }
+  constructor(private videoService :VideoService, private omniApiService : OmniApiService) {}
 
   ngOnInit() {
     this.videos = this.videoService.getVideos();
     this.videoService.videoDataReceived.subscribe( (videos :Video[]) => {
       this.videos = videos;
     });
+
+    this.selectedVideos = this.videoService.getSelectedVideos();
+    this.videoService.selectedVideosChanged.subscribe( ( selectedVideos :Video[] ) => {
+      this.selectedVideos = selectedVideos;
+    })
 
     this.videoService.apiLoading.subscribe( (state :string) => {
       this.loading = state;
@@ -30,17 +36,25 @@ export class JobSelectorComponent implements OnInit {
 
   onClick() {
     console.log(this.selectedVideos);
+    this.omniApiService.createDownloadTask( this.selectedVideos );
   }
 
-  onSelectVideo(video :Video) {
-    var indexVideo = this.selectedVideos.indexOf(video);
-    var indexId = this.selectedIds.indexOf(video.id);
-    if (indexId !== -1) {
-      this.selectedVideos.splice(indexVideo, 1);
-      this.selectedIds.splice(indexId, 1);
+  onSelectVideo( video :Video ) {
+    if ( this.selectedVideos.indexOf( video ) === -1 ) {
+      this.videoService.addSelectedVideo( video );
     } else {
-      this.selectedVideos.push(video);
-      this.selectedIds.push(video.id);
+      this.videoService.removeSelectedVideo( video );
     }
   }
+  // onSelectVideo(video :Video) {
+  //   var indexVideo = this.selectedVideos.indexOf(video);
+  //   var indexId = this.selectedIds.indexOf(video.id);
+  //   if (indexId !== -1) {
+  //     this.selectedVideos.splice(indexVideo, 1);
+  //     this.selectedIds.splice(indexId, 1);
+  //   } else {
+  //     this.selectedVideos.push(video);
+  //     this.selectedIds.push(video.id);
+  //   }
+  // }
 }
